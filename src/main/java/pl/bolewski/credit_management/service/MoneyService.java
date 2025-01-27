89215a28312c6a4f0d5883e6d3600d4289094338
dambolewski <dambolewski@gmail.com>
2025-01-27 18:03:17 +0100

    Changed getHistory order + set update on database instead of create-drop

--- src/main/java/pl/bolewski/credit_management/service/MoneyService.java
+++ src/main/java/pl/bolewski/credit_management/service/MoneyService.java
@@ -45,7 +45,7 @@ public class MoneyService {
 
     @Transactional(readOnly = true)
     public List<Money> getMoney(){
-        return moneyRepository.findAllByOrderByYearAscMonthDesc();
+        return moneyRepository.findAllByOrderByYearAscMonthAsc();
     }
 
     @Transactional(readOnly = true)
